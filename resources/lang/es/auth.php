e4a331351cb477c57f6d2c0008b8c26c12f4a72c
Leonel Lopez <leonellopez647@gmail.com>
2021-11-14 10:16:02 -0600

    Traducciones

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'Login' => 'Iniciar sesión',
+    'Forgot' => '¿Olvidaste tu contraseña?',
+    'Google' => 'Iniciar con Google',
+    'RGoogle' => 'Registrarse con Google',
+    'remember' => 'Recuerdame',
+    'email'=> 'Correo electrónico',
+    'Name'=>'Nombre',
+    'confirmPass'=>'Confirmar contraseña',
+    'Logout'=>'Cerrar sesión',
+    'Register' => 'Registrarse',
+    'failed' => 'Estas credenciales no se encuentran registradas.',
+    'throttle' => 'Demasiados intentos de inicio de sesión. Por favor inténtalo en :seconds segundos.',
+    'Password' => 'Contraseña',
+    'Reset'=>'Reiniciar contraseña',
+    'SendPasswordResetLink'=>'Envíar link de reinicio de contraseña',
+    'PassworBeforeContinuing'=>'Por favor confirma la contraseña antes de continuar.',
+    'VerifyYourEmailAddress'=>'Verifica tu correo electrónico',
+    'AFreshVerification'=>'Una nueva notificación ha sido enviada a tu correo electrónico',
+    'BeforeProceeding'=>'Antes de proceder, por favor revise su correo electrónico por una verificación',
+    'IfYouDidNotReceiveTheEmail'=>'Si no recibiste el correo electrónico',
+    'ClickHereToRequestAnother'=>'Presiona aquí para envíar otra petición',
+];
